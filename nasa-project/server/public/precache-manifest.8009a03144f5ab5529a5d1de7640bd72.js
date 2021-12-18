a574aee879d1b22d9d5c8415d52748f45056218c
Jugal Prajapati <59574693+Jugal-Prajapati-2505@users.noreply.github.com>
2021-12-18 15:10:44 +0530

    Add files via upload

---
+++ nasa-project/server/public/precache-manifest.8009a03144f5ab5529a5d1de7640bd72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3973616f9c2ef67a7c89b5768f6e0bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d881baa8d30562819d54",
+    "url": "/static/js/2.dd987ae3.chunk.js"
+  },
+  {
+    "revision": "7312af4a8b4834cf7ad71b51a68ab772",
+    "url": "/static/js/2.dd987ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c22869cc3671c48500",
+    "url": "/static/js/main.1e4ff9f2.chunk.js"
+  },
+  {
+    "revision": "4ccbded5fa8cde9125ae",
+    "url": "/static/js/runtime-main.ac2f9341.js"
+  }
+]);
---
+++ nasa-project/server/src/models/planets.model.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+const parse = require('csv-parse');
+
+const planets = require('./planets.mongo');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+/*
+const promise = new Promise((resolve, reject) => {
+    resolve(42);
+});
+promise.then((result) => {
+
+});
+const result = await promise;
+console.log(result);
+*/
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({ 
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', async (data) => {
+                if (isHabitablePlanet(data)) {
+                    // habitablePlanets.push(data);
+                    // await planets.create({
+                    //     keplerName: data.kepler_name,
+                    // });
+                }
+            })
+            .on('error', (err) => {
+                console.log(err);
+                reject(err);
+            })
+            .on('end', () => {
+                // console.log(habitablePlanets.map((planet) => {
+                //     return planet['kepler_name'];
+                // }));
+                console.log(`${habitablePlanets.length} habitable planets found!`);
+                resolve();
+            });
+    });
+}
+  
+// parse();
+
+async function getAllPlanets() {
+    return planets.find({});
+}
+
+module.exports = {
+    loadPlanetsData,
+    getAllPlanets,
+};
